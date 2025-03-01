a6b62d6b34835aa595f02dd1aebe5dc623eeb478
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2025-03-02 00:31:51 +0700

    Add code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Kelompok 7
+
+const fs = require('fs');
+const readline = require('readline');
+
+const filePath = 'database.json';
+
+// Membaca data dari JSON
+function loadData() {
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, '[]', 'utf8');
+    }
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
+}
+
+// Menyimpan data ke JSON
+function saveData(data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+}
